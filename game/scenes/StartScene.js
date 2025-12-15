a9f91d919802dfa1e023af6332ec928440f50649
nicktranuno <nicholastran@unomaha.edu>
2025-12-14 21:21:08 -0600

    changed a line

--- game/scenes/StartScene.js
+++ game/scenes/StartScene.js
@@ -1,29 +1,29 @@
 class StartScene extends Scene {
     start() {
         if (!StartScene.highScoreGO) {
-            StartScene.highScoreGO = new HighscoreGameObject();
-            StartScene.highScoreGO.transform.position = new Vector2(200, 100);
+            StartScene.highScoreGO = new HighscoreGameObject()
+            StartScene.highScoreGO.transform.position = new Vector2(200, 100)
         }
 
         // Instantiate starfield
         this.instantiate(new StarsGameObject())
         // Instantiate High Score
-        this.instantiate(StartScene.highScoreGO);
+        this.instantiate(StartScene.highScoreGO)
 
         // Start Button
-        const startButton = new GameObject("Start Button");
-        startButton.transform.position = new Vector2(250, 250);
-        startButton.addComponent(new Text());
-        const textComp = startButton.getComponent(Text);
-        textComp.text = "PLAY";
-        textComp.fillStyle = "red";
+        const startButton = new GameObject("Start Button")
+        startButton.transform.position = new Vector2(250, 250)
+        startButton.addComponent(new Text())
+        const textComp = startButton.getComponent(Text)
+        textComp.text = "PLAY"
+        textComp.fillStyle = "red"
 
         // load MainScene
         startButton.addComponent(new ButtonComponent(() => {
-            SceneManager.loadScene(new MainScene(StartScene.highScoreGO.getComponent(HighscoreController)));
-        }));
+            SceneManager.loadScene(new MainScene(StartScene.highScoreGO.getComponent(HighscoreController)))
+        }))
 
-        this.instantiate(startButton);
+        this.instantiate(startButton)
     }
 }
 
